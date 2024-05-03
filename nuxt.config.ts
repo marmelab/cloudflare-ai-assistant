5de5d94c88112940332e49a77ff6297ed2844127
Jonathan ARNAULT <jonathan@marmelab.com>
2024-05-03 09:34:28 +0200

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.21.1
      project name = ai-assistant
      framework = nuxt
      framework cli = nuxi@3.11.1
      package manager = npm@10.2.4
      wrangler = wrangler@3.52.0
      git = 2.34.1

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  nitro: {
+    preset: "cloudflare-pages"
+  },
+
+  modules: ["nitro-cloudflare-dev"]
+})
