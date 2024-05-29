3e2cbfc132bb21068d307d276b812177ab9e062f
Anthony Rimet <anthony.rimet03@gmail.com>
2024-05-29 11:44:25 +0200

    Feat(ui): Add chat structure ui

---
+++ components/BotMessage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex items-center gap-2">
+        <div class="avatar placeholder">
+            <div class="bg-neutral text-neutral-content rounded-full w-6">
+                <span class="text-xs">B</span>
+            </div>
+        </div>
+        <div class="bg-secondary dark:bg-gray-800 rounded-lg p-3 max-w-[70%]">
+            <p><slot></slot></p>
+        </div>
+    </div>
+</template>
---
+++ components/SenderMessage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex items-center justify-end gap-2">
+        <div class="bg-primary text-white rounded-lg p-3 max-w-[70%]">
+            <p>
+                <slot></slot>
+            </p>
+        </div>
+        <div class="avatar placeholder">
+            <div class="bg-neutral text-neutral-content rounded-full w-6">
+                <span class="text-xs">S</span>
+            </div>
+        </div>
+    </div>
+</template>
