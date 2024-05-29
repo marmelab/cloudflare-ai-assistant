9ecce93ed9eaaa0c27544b15a0556a156b61fc1e
Anthony Rimet <anthony.rimet03@gmail.com>
2024-05-29 10:28:13 +0200

    Feat(install): Add tailwind and daisyui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    head: {
-      charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1",
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+        },
     },
-  },
+    devtools: { enabled: true },
 
-  devtools: { enabled: true },
+    nitro: {
+        preset: 'cloudflare-pages',
+    },
 
-  nitro: {
-    preset: "cloudflare-pages",
-  },
+    css: ['~/assets/css/main.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
 
-  modules: ["nitro-cloudflare-dev"],
+    modules: ['nitro-cloudflare-dev'],
 });
